7a4886248b028cb3ad4bba7cee92b68dd4892b1b
Rishabh <rishabha.srivastava@nagarro.com>
2025-01-29 10:29:19 +0530

    deploying on backend

--- src/utils/axiosInstanceOpenAi.ts
+++ src/utils/axiosInstanceOpenAi.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstanceOpenAi = axios.create({
-    baseURL : 'http://localhost:3000'
+    baseURL : 'https://netflix-backend-topaz.vercel.app/'
 })
 
 axiosInstanceOpenAi.interceptors.request.use(
